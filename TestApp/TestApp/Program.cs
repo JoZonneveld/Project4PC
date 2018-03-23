58f59e6d945d95b34f3b11f2aaf93f10e180fc2b
Joost Zonneveld <joostz14@gmail.com>
2018-03-23 11:57:54 +0100

    test

---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (DB db = new DB())
+            {
+                db.get();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
