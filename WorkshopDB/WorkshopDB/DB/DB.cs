3fbbce1e32f479ca884f7b2afffde6708daf512e
Joost Zonneveld <joostz14@gmail.com>
2018-05-16 16:09:24 +0200

    last update

---
+++ WorkshopDB/WorkshopDB/DB/DB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace WorkshopDB.DB
+{
+    class DB
+    {
+        private SQLiteConnection DbConn;
+        public DB()
+        {
+            string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "dbTest.db3");
+        }
+    }
+}
